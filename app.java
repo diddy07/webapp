fbe797c198f5f4d9d4f3754773afe391964c9225
diddy07 <118678647+diddy07@users.noreply.github.com>
2022-11-24 01:04:40 +0000

    Update app.java

--- app.java
+++ app.java
@@ -2,5 +2,3 @@
 # Please do not hard code 
 # please make use of variables and dynamic variables
 # Author: Ndidiamaka Ejim 
-# congratulations i have been able to commit in this repository
-# Diddy, you are doing great
